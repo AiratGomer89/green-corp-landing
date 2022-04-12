4ef4640fa748882fb61a75506e281f739963a036
Airat <airat.gimranoff@gmail.com>
2022-04-13 02:35:37 +0300

    Добавили анимации при скроллинге и скролинг на ссылке

--- js/animations.js
+++ js/animations.js
@@ -23,7 +23,26 @@ function initIncreaseNumberAnimation() {
   increaseNumberAnimationStep(0, element, 5000);
 }
 
-initIncreaseNumberAnimation();
+
+let animationInited = false;
+
+function updateScroll() {
+  if (window.scrollY > 0) {
+    document.querySelector('header').classList.add('header__scrolled');
+  } else {
+    document.querySelector('header').classList.remove('header__scrolled');
+  }
+ 
+  // Запуск анимации увеличения числа
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation();
+  }
+}
+ 
+window.addEventListener('scroll', updateScroll);
 
 
 
@@ -46,3 +65,21 @@ document.querySelector('#budget').addEventListener('change', function handleSele
     document.querySelector('.form form').removeChild(otherInput);
   }
 });
+
+
+
+function addSmoothScroll(anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+ 
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+}
+ 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  addSmoothScroll(anchor);  
+});
+
+addSmoothScroll(document.querySelector('.more-button'));
